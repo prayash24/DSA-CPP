20117d35a6cdee98049461643696b73e6dfae623
prayash24 <prayashnaik24@gmail.com>
2025-08-12 22:46:05 +0530

    Create q2.cpp

---
+++ DAY 5/q2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5;
+
+    for (int i = 0; i < n; i++) {
+        // Spaces
+        for (int j = 0; j <= n - i - 1; j++) {
+            cout << " ";
+        }
+        // Stars
+        for (int j = 0; j < n; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
