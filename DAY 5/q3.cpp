24aed74f8f2da945d4023196977228d567b9e11b
prayash24 <prayashnaik24@gmail.com>
2025-08-12 22:46:09 +0530

    Create q3.cpp

---
+++ DAY 5/q3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5;
+
+    for (int i = 1; i <= n; i++) {
+        // Spaces
+        for (int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+
+        // Numbers backward
+        for (int j = i; j >= 1; j--) {
+            cout << j;
+        }
+
+        // Numbers forward
+        for (int j = 2; j <= i; j++) {
+            cout << j;
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
